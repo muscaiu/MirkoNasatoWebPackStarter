015e5e3cbf068aa11461e871f4ad1907a4f78c90
muscaiu <muscaiu@yahoo.com>
2016-10-26 16:53:34 +0300

    working!

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+import 'core-js'
+import 'reflect-metadata'
+import 'zone.js/dist/zone'
+
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppComponent } from './app/app.component'
+import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppComponent);
+platformBrowserDynamic().bootstrapModule(AppModule);
